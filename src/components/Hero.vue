834852f182561bb9b2e377786612d92985d9ac4c
peggy0418 <69634274+peggy0418@users.noreply.github.com>
2026-01-09 17:06:05 +0800

    fix: :fire: 微調樣式

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -13,9 +13,9 @@
       <div class="relative w-full h-full">
         <img src="../assets/pokemon.jpg" class="w-full h-full object-cover" />
         <div class="absolute inset-0 flex items-center justify-center">
-          <h1 class="text-4xl font-bold text-white drop-shadow-lg">
+          <!-- <h1 class="text-4xl font-bold text-white drop-shadow-lg">
             歡迎來到 Animate Shop
-          </h1>
+          </h1> -->
         </div>
       </div>
     </SwiperSlide>
@@ -25,7 +25,7 @@
       <div class="relative w-full h-full">
         <img src="../assets/pjsk.jpg" class="w-full h-full object-cover" />
         <div class="absolute inset-0 flex items-center justify-center">
-          <h1 class="text-4xl font-bold text-white drop-shadow-lg">限定商品販售中</h1>
+          <!-- <h1 class="text-4xl font-bold text-white drop-shadow-lg">限定商品販售中</h1> -->
         </div>
       </div>
     </SwiperSlide>
