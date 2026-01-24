37d565107ef4d099b219ab43b742f13d0adaf3fd
peggy0418 <69634274+peggy0418@users.noreply.github.com>
2026-01-25 00:51:57 +0800

    fix: :fire: 商品樣式微調

--- src/components/ProductGrid.vue
+++ src/components/ProductGrid.vue
@@ -2,55 +2,69 @@
   <div class="max-w-270 mx-auto px-2">
     <!-- 標題 -->
     <h2
-      class="relative text-xl font-bold mb-5 pl-8 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-5 before:h-[2px] before:bg-blue-600"
+      class="relative text-xl font-bold mb-16 pl-8 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-5 before:h-[2px] before:bg-blue-600"
     >
       隨機商品
     </h2>
     <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3">
-      <div
-        v-for="p in products"
-        :key="p.id"
-        class="border border-gray-200 p-2 hover:shadow-sm transition"
-      >
-        <img :src="p.image" class="w-full aspect-[3/4] object-cover mb-2" />
+      <div v-for="p in products" :key="p.id">
+        <div class="border border-gray-200 p-2 hover:shadow-sm transition relative group">
+          <!-- 特典標籤：固定在卡片最上方 -->
+          <div
+            v-if="p.hasBonus"
+            class="absolute -top-10 left-0 bg-white text-blue-600 text-[11px] font-semibold border border-blue-600 px-2 py-1 rounded shadow-md z-10"
+          >
+            贈送特典
+          </div>
 
-        <p class="text-xs leading-tight line-clamp-2 mb-1 text-gray-800">
-          {{ p.name }}
-        </p>
+          <div class="mb-2 mt-3">
+            <!-- 留空間給標籤 -->
+            <img :src="p.image" class="w-full aspect-[3/4] object-contain" />
+          </div>
 
-        <p class="text-sm font-bold text-blue-600">
-          {{ p.price }}
-        </p>
+          <p class="text-xs leading-tight line-clamp-2 mb-1 text-gray-800">
+            {{ p.name }}
+          </p>
+
+          <p class="text-sm font-bold text-blue-600">
+            {{ p.price }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import RandomImg from "../assets/random_img.jpg";
 const products = [
   {
     id: 1,
     name: "角色壓克力立牌",
-    price: "¥1,320",
-    image: "https://picsum.photos/300/400",
+    price: "2,200元(含稅)",
+    image: RandomImg,
+    hasBonus: true, // 有贈送特典
   },
   {
     id: 2,
-    name: "限定版漫畫 第1卷",
-    price: "¥2,200",
-    image: "https://picsum.photos/300/400",
+    name: "角色壓克力立牌",
+    price: "2,200元(含稅)",
+    image: RandomImg,
+    hasBonus: true, // 有贈送特典
   },
   {
     id: 3,
-    name: "角色徽章組",
-    price: "¥880",
-    image: "https://picsum.photos/300/400",
+    name: "角色壓克力立牌",
+    price: "2,200元(含稅)",
+    image: RandomImg,
+    hasBonus: false,
   },
   {
     id: 4,
-    name: "PVC 完成品模型",
-    price: "¥6,800",
-    image: "https://picsum.photos/300/400",
+    name: "角色壓克力立牌",
+    price: "2,200元(含稅)",
+    image: RandomImg,
+    hasBonus: false,
   },
 ];
 </script>
